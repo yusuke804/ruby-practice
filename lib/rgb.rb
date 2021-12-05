1f23c4dbfdcb0baacec7af2419eaa17220624273
yusuke804 <14au064f@al.rikkyo.ac.jp>
2021-12-05 20:14:50 +0900

    to_hexメソッドのリファクタリング② to_hexメソッドの完成

--- lib/rgb.rb
+++ lib/rgb.rb
@@ -1,9 +1,7 @@
 def to_hex(r,g,b)
-    hex = '#'
-    [r,g,b].each do |n|
-        hex += n.to_s(16).rjust(2, '0')
+    [r,g,b].inject('#') do |hex, n|
+        hex + n.to_s(16).rjust(2,'0')
     end
-    hex
 end
 
 
